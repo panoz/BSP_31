dc651c4547ec29592d1f8dce92a5410743106517
panos <panos@unreal>
2011-11-29 14:36:18 +0100

    second commit

---
+++ src/mensakassen/Customer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mensakassen;
+
+/**
+ *
+ * @author panos
+ */
+public interface Customer extends Runnable {
+    
+}
